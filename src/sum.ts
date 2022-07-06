d19aa00f9958abf8d66622169354e9b79964be2d
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2022-07-06 19:23:42 +0200

    Inline tests

--- src/sum.ts
+++ src/sum.ts
@@ -1,3 +1,23 @@
-export default (...numbers: number[]) => {
+const sum = (...numbers: number[]) => {
   return numbers.reduce((total, number) => total + number, 0);
 };
+
+export default sum;
+
+if (import.meta.vitest) {
+  const { describe, expect, it } = import.meta.vitest;
+
+  describe("sum", () => {
+    it("returns 0 with no numbers", () => {
+      expect(sum()).toBe(0);
+    });
+
+    it("returns same number with one number", () => {
+      expect(sum(5)).toBe(5);
+    });
+
+    it("sum with multiple numbers", () => {
+      expect(sum(1, 2, 3)).toBe(6);
+    });
+  });
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  // Disable including inline tests into production build
+  define: {
+    "import.meta.vitest": "undefined",
+  },
   test: {
+    includeSource: ["src/**/*.{js,ts}"],
     coverage: {
       reporter: ["text", "html"],
     },
